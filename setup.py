37d43948fb44cd9d99dd7903b396525fb5f4a86b
raja_charuvil <raja.asokan.charuvil@dbschenker.com>
2020-09-18 11:41:26 +0200

    updated license

--- setup.py
+++ setup.py
@@ -13,8 +13,8 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
-        "License :: OSI Approved :: GNU General Public License V2 or later (GPLv2+)",
-        "Oplerating System :: OS Independent"
+        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+        "Operating System :: OS Independent"
     ],
     long_description=long_description,
     long_description_content_type="text/markdown",
